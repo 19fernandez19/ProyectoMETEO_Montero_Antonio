65c60defb2e762a5242d45155668d11d609ef6ff
DavidMontero <davidmonterobellon11@gmail.com>
2021-11-04 10:26:08 +0100

    Estructura JFrame

---
+++ src/vista/VistaFrame.java
@@ -0,0 +1,58 @@
+package vista;
+
+import java.awt.BorderLayout;
+import java.awt.EventQueue;
+
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.border.EmptyBorder;
+import javax.swing.JLabel;
+import javax.swing.JButton;
+import javax.swing.ImageIcon;
+import javax.swing.SwingConstants;
+
+public class VistaFrame extends JFrame {
+
+	private JPanel contentPane;
+
+	/**
+	 * Launch the application.
+	 */
+	public static void main(String[] args) {
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					VistaFrame frame = new VistaFrame();
+					frame.setVisible(true);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
+	}
+
+	/**
+	 * Create the frame.
+	 */
+	public VistaFrame() {
+		getContentPane().setLayout(null);
+		
+		JPanel panel = new JPanel();
+		panel.setBounds(10, 10, 416, 243);
+		getContentPane().add(panel);
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		setBounds(100, 100, 723, 611);
+		contentPane = new JPanel();
+		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
+		setContentPane(contentPane);
+		contentPane.setLayout(null);
+		
+		JLabel imagen = new JLabel("");
+		imagen.setBounds(118, 33, 506, 498);
+		contentPane.add(imagen);
+		
+		
+		imagen.setIcon(new ImageIcon("C:\\Users\\david\\OneDrive\\Escritorio\\2º CFGS DAM\\Desarrollo de Interfaces\\Workspace Desarrollo de Interfaces\\ProyectoMeteo\\src\\imagenes\\mapa.png"));
+		
+	}
+}
