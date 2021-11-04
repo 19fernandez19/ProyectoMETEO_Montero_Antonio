a495a296aacd54e6f66ef0cc20cdabe7a5b407f7
19fernandez19 <anmufeber@gmail.com>
2021-11-04 09:49:08 +0100

    Properties

---
+++ src/FicheroProperties.java
@@ -0,0 +1,29 @@
+import java.io.*;
+import java.util.ArrayList;
+import java.util.Properties;
+
+public class FicheroProperties {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		Properties propiedades = new Properties();
+		InputStream entrada = null;
+		
+		
+		try {
+			entrada = new FileInputStream("datos.properties");
+			propiedades.load(entrada);//cargamos el fichero entrada
+			
+			System.out.println("El ID de la ciudad es " + propiedades);
+			System.out.println("La ciudad es " + propiedades.getProperty("localidad"));
+		
+		} catch (Exception e) {
+			e.printStackTrace();//mostrar los errores
+		}
+		
+		
+
+	}
+
+}
